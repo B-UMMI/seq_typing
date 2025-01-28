f324bb743b8013a06dad0006c8f0c22cbcef3c93
Miguel Machado <miguelpaulo.machado@biomerieux.com>
2025-01-28 18:38:14 +0000

    WIP split references

--- seqtyping/ecoli_stx_subtyping.py
+++ seqtyping/ecoli_stx_subtyping.py
@@ -71,21 +71,21 @@ def stx_subtype_parser(report_types, stx1_reference_file, stx2_reference_file, s
         String containing stx2 subtype, NT if seq_typing ran but no subtype could be determined, or NA if it didn't run.
         If extra subtypes were found, they will be present inside brackets separated by ;
     """
-    stx1_result = 'NT'
+    stx1_result = "NT"
     stx2_result_main = None
     stx2_result_other = []
-    with open(report_types, 'rt') as reader:
+    with open(report_types, "rt") as reader:
         for line in reader:
-            if not line.startswith('#'):
-                line = line.split('\t')
+            if not line.startswith("#"):
+                line = line.split("\t")
                 subtype = line[2]
                 if line[1] == stx1_reference_file:
-                    if line[0] == 'selected':
+                    if line[0] == "selected":
                         stx1_result = subtype
                 if line[1] == stx2_reference_file:
-                    if line[0] == 'selected':
+                    if line[0] == "selected":
                         stx2_result_main = subtype
-                    elif line[0] == 'other_probable_type':
+                    elif line[0] == "other_probable_type":
                         if float(line[4]) >= stx2_alternative_sequenced_covered and \
                                 float(line[6]) >= stx2_alternative_sequence_identity and \
                                 subtype != stx2_result_main and \
@@ -93,62 +93,88 @@ def stx_subtype_parser(report_types, stx1_reference_file, stx2_reference_file, s
                             stx2_result_other.append(subtype)
 
         if stx2_result_main is not None and len(stx2_result_other) > 0:
-            stx2_result_main = '{main}({other})'.format(main=stx2_result_main,
-                                                        other=';'.join(sorted(stx2_result_other)))
+            stx2_result_main = "{main}({other})".format(main=stx2_result_main,
+                                                        other=";".join(sorted(stx2_result_other)))
         elif stx2_result_main is None:
-            stx2_result_main = 'NT'
+            stx2_result_main = "NT"
 
     return stx1_result, stx2_result_main
 
 
 def main():
-    program_name = 'ecoli_stx_subtyping.py'
+    program_name = "ecoli_stx_subtyping.py"
 
     if sys.version_info[0] < 3:
-        sys.exit('Must be using Python 3. Try calling "python3 {}"'.format(program_name))
+        sys.exit("Must be using Python 3. Try calling \"python3 {}\"".format(program_name))
 
     parser, parser_reads, _, parser_assembly, _ = python_arguments(program_name=program_name, version=__version__)
-    parser.description = 'Gets E. coli stx subtypes'
+    parser.description = "Gets E. coli stx subtypes"
 
     # Add specific arguments
-    parser_reads.add_argument('--stx2covered', type=float,
-                              metavar='N',
-                              help='Minimal percentage of sequence covered to consider extra stx2'
-                                   ' subtypes (value between [0, 100])',
+    parser_reads.add_argument("--stx2Acovered", type=float,
+                              metavar="N",
+                              help="Minimal percentage of sequence covered to consider extra stx2A"
+                                   " subtypes (value between [0, 100])",
                               required=False, default=100)
-    parser_reads.add_argument('--stx2identity', type=float,
-                              metavar='N',
-                              help='Minimal sequence identity to consider extra stx2'
-                                   ' subtypes (value between [0, 100])',
+    parser_reads.add_argument("--stx2Aidentity", type=float,
+                              metavar="N",
+                              help="Minimal sequence identity to consider extra stx2A"
+                                   " subtypes (value between [0, 100])",
                               required=False, default=99.5)
 
-    parser_assembly.add_argument('--stx2covered', type=float,
-                                 metavar='N',
-                                 help='Minimal percentage of sequence covered to consider extra stx2'
-                                      ' subtypes (value between [0, 100])',
+    parser_assembly.add_argument("--stx2Acovered", type=float,
+                                 metavar="N",
+                                 help="Minimal percentage of sequence covered to consider extra stx2A"
+                                      " subtypes (value between [0, 100])",
                                  required=False, default=100)
-    parser_assembly.add_argument('--stx2identity', type=float,
-                                 metavar='N',
-                                 help='Minimal sequence identity to consider extra stx2'
-                                      ' subtypes (value between [0, 100])',
+    parser_assembly.add_argument("--stx2Aidentity", type=float,
+                                 metavar="N",
+                                 help="Minimal sequence identity to consider extra stx2A"
+                                      " subtypes (value between [0, 100])",
                                  required=False, default=99.5)
+
+    parser_reads.add_argument("--stx2Bcovered", type=float,
+                              metavar="N",
+                              help="Minimal percentage of sequence covered to consider extra stx2B"
+                                   " subtypes (value between [0, 100])",
+                              required=False, default=95)
+    parser_reads.add_argument("--stx2Bidentity", type=float,
+                              metavar="N",
+                              help="Minimal sequence identity to consider extra stx2B"
+                                   " subtypes (value between [0, 100])",
+                              required=False, default=95)
+
+    parser_assembly.add_argument("--stx2Bcovered", type=float,
+                                 metavar="N",
+                                 help="Minimal percentage of sequence covered to consider extra stx2B"
+                                      " subtypes (value between [0, 100])",
+                                 required=False, default=95)
+    parser_assembly.add_argument("--stx2Bidentity", type=float,
+                                 metavar="N",
+                                 help="Minimal sequence identity to consider extra stx2B"
+                                      " subtypes (value between [0, 100])",
+                                 required=False, default=95)
 
     args = parser.parse_args()
 
     msg = []
     if args.minGeneCoverage < 0 or args.minGeneCoverage > 100:
-        msg.append('--minGeneCoverage should be a value between [0, 100]')
+        msg.append("--minGeneCoverage should be a value between [0, 100]")
     # if args.minGeneIdentity < 0 or args.minGeneIdentity > 100:
-    #     msg.append('--minGeneIdentity should be a value between [0, 100]')
-    if args.stx2covered < 0 or args.stx2covered > 100:
-        msg.append('--stx2covered should be a value between [0, 100]')
-    if args.stx2identity < 0 or args.stx2identity > 100:
-        msg.append('--stx2identity should be a value between [0, 100]')
-    if args.blast is None and args.org != ['stx', 'subtyping']:
-        msg.append('Use "--org stx subtyping" with {}'.format(program_name))
+    #     msg.append("--minGeneIdentity should be a value between [0, 100]")
+    if args.stx2Acovered < 0 or args.stx2Acovered > 100:
+        msg.append("--stx2Acovered should be a value between [0, 100]")
+    if args.stx2Aidentity < 0 or args.stx2Aidentity > 100:
+        msg.append("--stx2Aidentity should be a value between [0, 100]")
+    if args.stx2Bcovered < 0 or args.stx2Bcovered > 100:
+        msg.append("--stx2Bcovered should be a value between [0, 100]")
+    if args.stx2Bidentity < 0 or args.stx2Bidentity > 100:
+        msg.append("--stx2Bidentity should be a value between [0, 100]")
+    if args.blast is None and args.org != ["stx", "subtyping"]:
+        msg.append("Use \"--org stx subtyping\" with {}".format(program_name))
 
     if len(msg) > 0:
-        argparse.ArgumentParser(prog='{} options'.format(program_name)).error('\n'.join(msg))
+        argparse.ArgumentParser(prog="{} options".format(program_name)).error("\n".join(msg))
 
     start_time = time.time()
 
@@ -157,15 +183,15 @@ def main():
         os.makedirs(args.outdir)
 
     # Start logger
-    logfile, time_str = utils.start_logger(args.outdir)
+    logfile, _ = utils.start_logger(args.outdir)
 
     _ = utils.general_information(script_name=program_name, logfile=logfile, version=__version__)
-    print('\n')
+    print("\n")
 
     folders_2_remove = []
 
     # Create modules pickles folder
-    pickles_folder = os.path.join(args.outdir, 'pickles', '')
+    pickles_folder = os.path.join(args.outdir, "pickles", "")
     if not os.path.isdir(pickles_folder):
         os.makedirs(pickles_folder)
     folders_2_remove.append(pickles_folder)
@@ -175,10 +201,10 @@ def main():
         args.func(args)
     folders_2_remove.extend(folders_2_remove_func)
 
-    min_gene_identity = args_config_dict['minimum_gene_identity']
-    min_gene_coverage = args_config_dict['minimum_gene_coverage']
-    type_separator = args_config_dict['type_separator']
-    length_extra_seq = args_config_dict['length_extra_seq']
+    min_gene_identity = args_config_dict["minimum_gene_identity"]
+    min_gene_coverage = args_config_dict["minimum_gene_coverage"]
+    type_separator = args_config_dict["type_separator"]
+    length_extra_seq = args_config_dict["length_extra_seq"]
 
     type_in_new = not args.typeNotInNew
 
@@ -190,26 +216,35 @@ def main():
                                                 min_identity=min_gene_identity,
                                                 type_in_new=type_in_new)
 
-    stx1_result, stx2_result = stx_subtype_parser(
-        os.path.join(args.outdir, 'seq_typing.report_types.tab'),
-        [ref_file for ref_file in reference if 'stx1' in os.path.basename(ref_file).lower()][0],
-        [ref_file for ref_file in reference if 'stx2' in os.path.basename(ref_file).lower()][0],
-        args.stx2covered, args.stx2identity)
+    stx1A_result, stx2A_result = stx_subtype_parser(
+        os.path.join(args.outdir, "seq_typing.report_types.tab"),
+        [ref_file for ref_file in reference if "stx1A" in os.path.basename(ref_file).lower()][0],
+        [ref_file for ref_file in reference if "stx2A" in os.path.basename(ref_file).lower()][0],
+        args.stx2Acovered, args.stx2Aidentity)
+
+    stx1B_result, stx2B_result = stx_subtype_parser(
+        os.path.join(args.outdir, "seq_typing.report_types.tab"),
+        [ref_file for ref_file in reference if "stx1B" in os.path.basename(ref_file).lower()][0],
+        [ref_file for ref_file in reference if "stx2B" in os.path.basename(ref_file).lower()][0],
+        args.stx2Bcovered, args.stx2Bidentity)
 
     # Rename the file to keep ecoli_stx_subtyping stamp
-    if os.path.isfile(os.path.join(args.outdir, 'seq_typing.report_types.tab')):
-        os.rename(os.path.join(args.outdir, 'seq_typing.report_types.tab'),
-                  os.path.join(args.outdir, 'seq_typing.ecoli_stx_subtyping.report_types.tab'))
+    if os.path.isfile(os.path.join(args.outdir, "seq_typing.report_types.tab")):
+        os.rename(os.path.join(args.outdir, "seq_typing.report_types.tab"),
+                  os.path.join(args.outdir, "seq_typing.ecoli_stx_subtyping.report_types.tab"))
 
     # Remove the file to only keep the ecoli_stx_subtyping one
-    if os.path.isfile(os.path.join(args.outdir, 'seq_typing.report.txt')):
-        os.remove(os.path.join(args.outdir, 'seq_typing.report.txt'))
+    if os.path.isfile(os.path.join(args.outdir, "seq_typing.report.txt")):
+        os.remove(os.path.join(args.outdir, "seq_typing.report.txt"))
 
-    print('\n'
-          'E. coli stx_subtyping - {stx1_result}:{stx2_result}\n'
-          '\n'.format(stx1_result=stx1_result, stx2_result=stx2_result))
-    with open(os.path.join(args.outdir, 'seq_typing.ecoli_stx_subtyping.txt'), 'wt') as writer:
-        writer.write(':'.join([stx1_result, stx2_result]))
+    stx_subtype_str = ":".join([stx1A_result, stx1B_result, stx2A_result, stx2B_result])
+    print(
+        "\n"
+        f"E. coli stx_subtyping - {stx_subtype_str}\n"
+        "\n"
+    )
+    with open(os.path.join(args.outdir, "seq_typing.ecoli_stx_subtyping.txt"), "wt", encoding="utf-8") as writer:
+        print(stx_subtype_str, file=writer)
 
     if not args.debug:
         for folder in folders_2_remove:
